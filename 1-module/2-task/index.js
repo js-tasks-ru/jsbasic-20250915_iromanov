57a060a5b72608bfd6149d9706ce1d24d5c9f955
ivangrozav-beep <ivangrozav@gmail.com>
2025-09-24 17:45:01 +0300

    да так лучше решить 1.2, добавил решение для 1.3

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -11,16 +11,7 @@ function print(text) {
  */
 function isValid(name) {
   // ваш код...
-// Регулярное выражение для проверки:
-  // ^ - начало строки
-  // \S - любой непробельный символ
-  // {4,} - минимум 4 символа
-  // $ - конец строки
-   if (name == null) {
-     return false;
-    }
-  const regex = /^\S{4,}$/;
-  return regex.test(name);
+   return !!name && !name.includes(' ') && name.length >= 4;
 }
 /**
  * Эту функцию трогать не нужно
