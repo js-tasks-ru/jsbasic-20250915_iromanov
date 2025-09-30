d1e9b1db697c4f80be6ba5c1bb6f4edff6d4685c
ivangrozav-beep <ivangrozav@gmail.com>
2025-09-30 16:32:23 +0300

    решил 3.1, 3.2, 3.3

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,11 @@
 function filterRange(arr, a, b) {
   // ваш код...
+    const filteredArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+     if (element >= a && element <= b) {
+      filteredArr.push(element);
+    }
+  }
+  return filteredArr;
 }
